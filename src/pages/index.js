e57a0a925766e8d5885bcd2555448de4af6c0803
EkaterinaMash <ekaterina.mashinskayaa@gmail.com>
2023-11-22 11:30:43 +0200

    feat: create tasks-management page

---
+++ src/pages/index.js
@@ -0,0 +1,27 @@
+import styles from '../styles/index.module.scss'
+import {getIndexPage} from "../../lib/api";
+import Tasks from "@/components/tasks/tasks";
+import Sidebar from "@/components/sidebar/sidebar";
+
+function Index(props) {
+    const tasksData = props;
+
+    return (
+        <main className={styles.main}>
+            <section className={styles.main__content}>
+                <Tasks tasksData={tasksData}/>
+            </section>
+        </main>
+    )
+}
+
+export const getStaticProps = async () => {
+    const indexPage = await getIndexPage();
+
+    return {
+        props: indexPage,
+        revalidate: 1
+    };
+};
+
+export default Index;
